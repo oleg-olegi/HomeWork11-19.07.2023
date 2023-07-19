5cbef3821a233aff4c0f8697d45d0d4e783049ba
User <ol.shink87@gmail.com>
2023-07-19 22:58:31 +0300

    задача2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.time.LocalDate;
 import java.util.Scanner;
+
 public class Main {
     public static void checkLeapYear(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
@@ -7,15 +9,45 @@ public class Main {
             System.out.println(year + " год не является високосным");
         }
     }
-        public static void main (String[]args){
-            task1();
 
-        }
-        public static void task1 () {
-            System.out.println("Задача 1");
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Введите год");
-            int leapYear = sc.nextInt();
-            checkLeapYear(leapYear);
+    public static void getOS(int os, int year) {
+        int currentYear = LocalDate.now().getYear();
+        if (os == 0) {
+            if (year < currentYear) {
+                System.out.println("Установите облегченную версию для iOS");
+            } else {
+                System.out.println("Установите обычную версию для iOS");
+            }
+        } else if (os == 1) {
+            if (year < currentYear) {
+                System.out.println("Установите облегченную версию для Android");
+            } else {
+                System.out.println("Установите обычную версию для Android");
+            }
         }
     }
+
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите год");
+        int leapYear = sc.nextInt();
+        checkLeapYear(leapYear);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите вашу ОС: 0 - iOS, 1 - Android");
+        int os = sc.nextInt();
+        System.out.println("Введите год вашего устройства");
+        int deviceYear = sc.nextInt();
+        getOS(os, deviceYear);
+    }
+
+}
