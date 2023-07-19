d82e003ff9032da1eae6b4570d8e4c193ea3824c
User <ol.shink87@gmail.com>
2023-07-19 23:16:20 +0300

    задача3

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,9 @@ public class Main {
 
     public static void getOS(int os, int year) {
         int currentYear = LocalDate.now().getYear();
+        if (os!=0 || os!=1){
+            throw new RuntimeException("Некорректные данные");
+        }
         if (os == 0) {
             if (year < currentYear) {
                 System.out.println("Установите облегченную версию для iOS");
@@ -26,10 +29,21 @@ public class Main {
             }
         }
     }
-
+public static int  checkDeliveryDistance (int distance){
+    if (distance <= 20) {
+       return 1;// System.out.println("Доставка одни сутки");
+    } else if (distance > 20 && distance <= 60) {
+        return 2;//System.out.println("Доставка двое суток");
+    } else if (distance > 60 && distance <= 100) {
+        return 3;//System.out.println("Доставка трое суток");
+    } else {
+        return 0; //System.out.println("Доставки нет");
+    }
+}
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -50,4 +64,9 @@ public class Main {
         getOS(os, deviceYear);
     }
 
+    public static void task3() {
+        System.out.println("Задача 3");
+        int distance = 52;
+        System.out.println("Доставка " +checkDeliveryDistance(distance) + " дней");
+    }
 }
